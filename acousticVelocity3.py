5a7458b3c732a13fb05979ae6e8c338437bf4386
Kreiva Laboratories <kreivalabs@gmail.com>
2017-10-27 21:50:50 -0700

    Syntax for Python 3

---
+++ acousticVelocity3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+temp_fahrenheit = float(input("Enter temperture in degrees Fahrenheit: "))
+temp_celsius = (temp_fahrenheit - 32) / 1.8      
+meters_seconds = (temp_celsius * 0.6) + 331.4    
+feet_milliseconds = meters_seconds * 0.00328084  
+
+print("Approximate acoustic velocity is", meters_seconds, "m/s, or,", feet_milliseconds, "ft/ms.")
+print("Press <Enter> to exit.")
+input()
