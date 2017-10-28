66226249b8e840e2c3e4d769c9c2edecafcaca7a
Kreiva Laboratories <kreivalabs@gmail.com>
2017-10-27 21:49:25 -0700

    Create acousticVelocity.py

---
+++ acousticVelocity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#uses Future addition
+
+from __future__ import print_function
+from builtins import input
+temp_fahrenheit = float(input("Enter temperture in degrees Fahrenheit: "))
+temp_celsius = (temp_fahrenheit - 32) / 1.8      
+meters_seconds = (temp_celsius * 0.6) + 331.4    
+feet_milliseconds = meters_seconds * 0.00328084  
+
+print('Approximate acoustic velocity is', meters_seconds, 'm/s, or,', feet_milliseconds, 'ft/ms.') 
+print('Press <Enter> to exit.')
+input()
