22c69e9265d6f8050b043488b9651e7934def0be
Kreiva Laboratories <kreivalabs@gmail.com>
2017-10-29 12:58:34 -0700

    Add user prompt and delay calculation
    Adds an additional user prompt for measured distance between speaker systems and returns the resulting detail time. Adds line comments.
    Adds note regarding ideal gas law - this calculation cannot take into account air pressure or molecular density for high-accuracy calculations. Results are sufficient for indoor theater or small to mid-sized concert use.

--- acousticVelocity.py
+++ acousticVelocity.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python
-#uses Future addition
 
-from __future__ import print_function
+#Calculate acoustic velocity in air based on temperature. Calculate resulting delay time
+#in milliseconds for a measured distance. 
+#NOTE: there is no method for factoring in gas density in air. Returned values are suitable
+#for indoor use where temperature and humidity swings are not extreme.
+
+#use Future
+from __future__ import absolute_import, division, print_function, unicode_literals
 from builtins import input
+
+#prompt for user input
 temp_fahrenheit = float(input("Enter temperture in degrees Fahrenheit: "))
+measured_distance = float(input("Enter measured distance between speakers in feet: "))
+
+#convert Fahrenheit to degrees Celsius
 temp_celsius = (temp_fahrenheit - 32) / 1.8      
+
+#calculate acoustic velocity in meters/second
 meters_seconds = (temp_celsius * 0.6) + 331.4    
+
+#convert meters/second to feet/millisecond
 feet_milliseconds = meters_seconds * 0.00328084  
 
+#calculate time differential based on acoustic velocity and measured distance
+delay_time = measured_distance / feet_milliseconds
+
+#display results
 print('Approximate acoustic velocity is', meters_seconds, 'm/s, or,', feet_milliseconds, 'ft/ms.') 
+print('Approximate delay time is', delay_time, 'm/s.')
 print('Press <Enter> to exit.')
 input()
