5c5326b271faf4332058978977ead64d031a93c7
Kreiva Laboratories <kreivalabs@gmail.com>
2017-10-29 13:01:01 -0700

    Add additional user prompt and delay calculation
    Adds an additional user prompt for measured distance in feet between speaker systems, returns calculated delay in milliseconds.

--- acousticVelocity3.py
+++ acousticVelocity3.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python
 
+#prompt for user input
 temp_fahrenheit = float(input("Enter temperture in degrees Fahrenheit: "))
+measured_distance = float(input("Enter measured distance between speakers in feet: "))
+
+#convert Fahrenheit to degrees Celsius
 temp_celsius = (temp_fahrenheit - 32) / 1.8      
+
+#calculate acoustic velocity in meters/second
 meters_seconds = (temp_celsius * 0.6) + 331.4    
+
+#convert meters/second to feet/millisecond
 feet_milliseconds = meters_seconds * 0.00328084  
 
-print("Approximate acoustic velocity is", meters_seconds, "m/s, or,", feet_milliseconds, "ft/ms.")
-print("Press <Enter> to exit.")
+#calculate time differential based on acoustic velocity and measured distance
+delay_time = measured_distance / feet_milliseconds
+
+#display results
+print('Approximate acoustic velocity is', meters_seconds, 'm/s, or,', feet_milliseconds, 'ft/ms.')
+print('Approximate delay time is', delay_time, 'ms.')
+print('Press Enter key to exit.')
 input()
