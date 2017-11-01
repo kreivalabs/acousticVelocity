8dc2f62d18e970f250403ad5830cc352bd622e23
Kreiva Laboratories <kreivalabs@gmail.com>
2017-10-31 17:21:49 -0700

    Version 1.1 - Update comment formatting

--- acousticVelocityAndDelayUsingFuture.py
+++ acousticVelocityAndDelayUsingFuture.py
@@ -9,27 +9,27 @@
 
 #This script uses Future additions from http://www.python-future.org
 
-   #import Future additions
+#import Future additions
 from __future__ import absolute_import, division, print_function, unicode_literals
 from builtins import input
 
-   #prompt for user input
+#prompt for user input
 temp_fahrenheit = float(input("Enter temperture in degrees Fahrenheit: "))
 measured_distance = float(input("Enter measured distance between speakers in feet: "))
 
-   #convert Fahrenheit to degrees Celsius
+#convert Fahrenheit to degrees Celsius
 temp_celsius = (temp_fahrenheit - 32) / 1.8      
 
-   #calculate acoustic velocity in meters/second
+#calculate acoustic velocity in meters/second
 meters_seconds = (temp_celsius * 0.6) + 331.4    
 
-   #convert meters/second to feet/millisecond
+#convert meters/second to feet/millisecond
 feet_milliseconds = meters_seconds * 0.00328084  
 
-   #calculate time differential based on acoustic velocity and measured distance
+#calculate time differential based on acoustic velocity and measured distance
 delay_time = measured_distance / feet_milliseconds
 
-   #display results
+#display results
 print('Approximate acoustic velocity is', meters_seconds, 'm/s, or,', feet_milliseconds, 'ft/ms.') 
 print('Approximate delay time is', delay_time, 'm/s.')
 print('Press <Enter> to exit.')
