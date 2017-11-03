21164a83a78d2474c47456825f704ac83871429c
Kreiva Laboratories <kreivalabs@gmail.com>
2017-11-02 19:57:17 -0700

    Update to version 1.3
    Adds rounding function, returning results with two decimal places. This is sufficient for most loudspeaker processing hardware and software.

--- acousticVelocityAndDelayUsingFuture.py
+++ acousticVelocityAndDelayUsingFuture.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
-#versions 1.2 / 01-November-2017
+#versions 1.3 / 02-November-2017
 
-#Calculate acoustic velocity in air based on temperature. Calculate resulting delay time
-#in milliseconds for a measured distance. 
-#NOTE: there is no method for factoring in gas density in air. Returned values are suitable
-#for indoor use where temperature and humidity swings are not extreme.
+# Calculate acoustic velocity in air based on temperature. Calculate resulting delay time
+# in milliseconds for a measured distance. Results rounded to two decimal places.
+# NOTE: there is no method for factoring in gas density in air. Returned values are suitable
+# for indoor use where temperature and humidity swings are not extreme.
 
-#This script uses Future additions from http://www.python-future.org
+# This script uses Future additions from http://www.python-future.org
 
-#import Future additions
+# Import Future additions
 from __future__ import absolute_import, division, print_function, unicode_literals
 from builtins import input
 
@@ -17,29 +17,33 @@ title='Acoustic Velocity and Loudspeaker Delay Calculator'
 print(title)
 print('=' * 80)
 
-#prompt for user input
+# Prompt for user input
 temp_fahrenheit = float(input("Enter temperture in degrees Fahrenheit: "))
 measured_distance = float(input("Enter measured distance between speakers in feet: "))
 
-#convert Fahrenheit to degrees Celsius
+# Convert Fahrenheit to degrees Celsius
 temp_celsius = (temp_fahrenheit - 32) / 1.8      
 
-#calculate acoustic velocity in meters/second
+# Calculate acoustic velocity in meters/second
 meters_seconds = (temp_celsius * 0.6) + 331.4    
 
-#convert meters/second to feet/millisecond
+# Convert meters/second to feet/millisecond
 feet_milliseconds = meters_seconds * 0.00328084  
 
-#calculate time differential based on acoustic velocity and measured distance
+# Calculate time differential based on acoustic velocity and measured distance
 delay_time = measured_distance / feet_milliseconds
 
-#display results
+# Round returned values to two decimal places - suitable for most loudspeaker processing hardware and software.
+meters_seconds_round = str(round(meters_seconds, 2))
+feet_milliseconds_round = str(round(feet_milliseconds, 2))
+delay_time_round = str(round(delay_time, 2))
+
+# Display results
 print('=' * 80)
 print('Approximate acoustic velocity is:') 
-print(meters_seconds, 'm/s, or,') 
-print(feet_milliseconds, 'ft/ms.')
+print(meters_seconds_round, 'm/s, or', feet_milliseconds, 'ft/ms.')
 print()
-print('Approximate delay time is', delay_time, 'm/s.')
+print('Approximate delay time is', delay_time_round, 'ms.')
 print()
 print('Press <Enter> to exit.')
 input()
